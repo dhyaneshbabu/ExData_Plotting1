8f4d352a10cb5d3cfb8a7e322fa5a9c8d670891a
dhyaneshbabu <dhyaneshbabu@gmail.com>
2018-03-18 23:31:11 +0530

    plot 4 modifie

--- project 2/E2Plot4.R
+++ project 2/E2Plot4.R
@@ -7,9 +7,9 @@ SCC <- readRDS("Source_Classification_Code.rds")
 coal_scc <- SCC[grep("coal",SCC$Short.Name,ignore.case = TRUE),"SCC"]
 NEI_coal<- NEI[which(NEI$SCC %in% coal_scc),]
 
-g <- ggplot(NEI_coal,aes((year),Emissions/(10^5)))
-g+geom_bar(stat = "identity")+ labs(y="Emissions from coal combustion-related sources (10^5)",x="Year (1999 - 2008)")
+g <- ggplot(NEI_coal,aes(factor(year),Emissions/(10^5)))
+my_plot <- g+geom_bar(stat = "identity")+ labs(y="Emissions from coal combustion-related sources (10^5)",x="Year (1999 - 2008)")
 
+print(my_plot)
 
-dev.copy(png, file="E2plot4.png", height=480, width=480)
-dev.off()
+ggsave("E2plot4.png", plot = my_plot) 
