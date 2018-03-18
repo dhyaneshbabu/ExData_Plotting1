879beba1e3a1eac270c64c28d6245d6ac49588a9
dhyaneshbabu <dhyaneshbabu@gmail.com>
2018-03-18 20:27:11 +0530

    plot1.R

--- plot1.R
+++ plot1.R
@@ -1,10 +1,21 @@
-
 library(dplyr) 
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile="data.zip", method="curl")
+unzip ("data.zip")
+
 dataPath <- "exdata_data_household_power_consumption"
-training_Subjects <- read.table(file.path(dataPath, "household_power_consumption.txt"),sep=";",header = TRUE)
-head(training_Subjects)
 
-training_Subjects$Date <- as.Date( as.character(training_Subjects$Date), "%d-%m-%y")
+full_power_consumption <- read.table(file.path( "household_power_consumption.txt"),sep=";", na.strings="?" ,header = TRUE)
 
-filtered_DF <- filter(training_Subjects, Date ==as.Date("01/02/2007")  )
-head(filtered_DF)
+#head(full_power_consumption)
+#dim(full_power_consumption)
+
+#filter based on the date 
+filtered_full_power_consumption <- full_power_consumption[(full_power_consumption$Date=="1/2/2007" | full_power_consumption$Date=="2/2/2007" ), ]
+
+#head(filtered_full_power_consumption)
+#dim(filtered_full_power_consumption)
+
+hist(filtered_full_power_consumption$Global_active_power,col="red",main="Global active power",xlab ="Global active power (Kilowatt)" ,cex.sub=0.8)
+
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
